b00ec63cd86b091f257c91079e3d3246a4789361
Extracat <>
2019-01-17 09:49:03 +0300

    test

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
-<?php require $_SERVER['DOCUMENT_ROOT']."/index.html";
+<?php //require $_SERVER['DOCUMENT_ROOT']."/index.html";
 // header( 'Location: /index.html' );  
+print $_SERVER [ "HTTP_ACCEPT_LANGUAGE" ];
 ?>
