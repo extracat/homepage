8de13ee18fab6258bdeefe88ec729e80dd6685cd
Extracat <>
2019-01-17 10:09:05 +0300

    lang test

--- index.php
+++ index.php
@@ -1,4 +1,46 @@
 <?php //require $_SERVER['DOCUMENT_ROOT']."/index.html";
-// header( 'Location: /index.html' );  
-print $_SERVER [ "HTTP_ACCEPT_LANGUAGE" ];
+// header( 'Location: /index.html' ); 
+
+public function __construct()
+    {
+        if (($list = strtolower($_SERVER['HTTP_ACCEPT_LANGUAGE']))) {
+            if (preg_match_all('/([a-z]{1,8}(?:-[a-z]{1,8})?)(?:;q=([0-9.]+))?/', $list, $list)) {
+                $this->language = array_combine($list[1], $list[2]);
+                foreach ($this->language as $n => $v)
+                    $this->language[$n] = $v ? $v : 1;
+                arsort($this->language, SORT_NUMERIC);
+            }
+        } else $this->language = array();
+    }
+
+
+$langs=array(
+            'ru'=>array('ru','be','uk','ky','ab','mo','et','lv'),
+            'de'=>'de',
+            'en'=>'en'
+        );
+
+
+ public function getBestMatch($default, $langs)
+    {
+        $languages=array();
+        foreach ($langs as $lang => $alias) {
+            if (is_array($alias)) {
+                foreach ($alias as $alias_lang) {
+                    $languages[strtolower($alias_lang)] = strtolower($lang);
+                }
+            }else $languages[strtolower($alias)]=strtolower($lang);
+        }
+
+        foreach ($this->language as $l => $v) {
+            $s = strtok($l, '-'); // убираем то что идет после тире в языках вида "en-us, ru-ru"
+            if (isset($languages[$s]))
+                return $languages[$s];
+        }
+        return $default;
+    }
+
+print getBestMatch('en', $langs);
+
+
 ?>
