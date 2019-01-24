a852edda29106f23e8a8392e6a9250df6ec9a608
Extracat <>
2019-01-24 10:23:41 +0300

    update

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ else
 var server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Hello World!\n');
 });
 
 server.listen(port, hostname, () => {
