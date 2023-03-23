6802883f7635cba9241ab82cea1ddbd39aba90f0
Tony <extracat@users.noreply.github.com>
2023-03-23 14:49:53 +0900

    added Yandex Metrika

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
+import YandexMetrika from '../components/YandexMetrika';
 
 export default function Document() {
   return (
@@ -14,6 +15,7 @@ export default function Document() {
       </Head>
       <body>
         <script type="text/javascript" src='/noflash.js' />
+        <YandexMetrika yid={"92706094"} webvisor={false} />
         <Main />
         <NextScript />
       </body>
