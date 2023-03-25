09eb644680944a5c4ba3b20c635affb517717dea
Tony <extracat@users.noreply.github.com>
2023-03-25 14:59:53 +0900

    card images added + timetracker new assets

--- components/cards.js
+++ components/cards.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import posts from '../pages/posts.json';
+import Image from 'next/image'
 
 export function Cards() {
   return (
@@ -7,7 +8,8 @@ export function Cards() {
       <div className="grid">
         {posts.posts.map((p) => (
           <Link key={p.slug} href={p.slug} className="card">
-            <div className="text">
+            { p.image != "" ? <Image className="card-image no-bleed" src={p.image} width={1200} height={630} /> : ''}
+            <div className="card-text">
               <h3>{p.title}</h3>
               <p>{p.excerpt}</p>
             </div>
