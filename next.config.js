166cb20311d8fe390e3bcf767ea74bb833929311
Tony <extracat@users.noreply.github.com>
2023-05-16 11:25:20 +0900

    image optimization turned off

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const withNextra = require('nextra')({
   theme: './components/theme.tsx',
+  staticImage: true,
   latex: true
 })
 
 module.exports = withNextra({
   // Other Next.js configurations
-
+  images: {
+    unoptimized: true,
+  }
 })
